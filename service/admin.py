9e5d5d7c9497077aa40cf06eb632a211fc8d9257
nalmutairi <almutairi.nm@gmail.com>
2019-01-29 18:27:15 +0300

    yay

--- service/admin.py
+++ service/admin.py
@@ -4,6 +4,9 @@ from .models import Company, Slot, Day, Appointment, Profile
 
 class SlotAdmin(admin.ModelAdmin):
 	list_display = ['day', 'start_time']
+
+
+	
 admin.site.register(Company)
 admin.site.register(Slot, SlotAdmin)
 admin.site.register(Day)
