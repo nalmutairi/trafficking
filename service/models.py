a0fcbfe263bd055062775162df3cca1886e9b978
nalmutairi <almutairi.nm@gmail.com>
2019-02-10 17:52:12 +0300

    company rates and slot information

--- service/models.py
+++ service/models.py
@@ -15,6 +15,7 @@ class Company(models.Model):
 	desc = models.TextField()
 	logo = models.ImageField(null = True, blank = True)
 	slogan = models.CharField(max_length = 300, blank = True, null = True)
+	rate = models.DecimalField(max_digits=4, decimal_places=2, blank = True, null = True)
 	
 	def __str__(self):
 		return self.name
@@ -24,15 +25,6 @@ class Day(models.Model):
 	opening_time = models.TimeField()
 	closing_time = models.TimeField()
 	company = models.ForeignKey(Company, on_delete= models.CASCADE, related_name = 'days')
-
-
-
-class Slot(models.Model):
-	user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'appointments', null=True, blank=True)
-	day = models.ForeignKey(Day, on_delete= models.CASCADE, related_name='slots')
-	start_time = models.TimeField()
-	end_time = models.TimeField()
-
 
 class Address(models.Model):
 	user = models.ForeignKey(User, on_delete= models.CASCADE, related_name='addresses')
@@ -45,6 +37,23 @@ class Address(models.Model):
 
 
 	def __str__(self):
-		return self.area
+		return self.area 
+
+class Slot(models.Model):
+	STATUS_CHOICE = (
+		('O', 'Open'),
+		('R', 'Reserved'),
+		('C' , 'Confirmed')
+		)
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'appointments', null=True, blank=True)
+	address = models.ForeignKey(Address, on_delete = models.CASCADE, null = True, blank = True)
+	day = models.ForeignKey(Day, on_delete= models.CASCADE, related_name='slots')
+	start_time = models.TimeField()
+	end_time = models.TimeField()
+	status = models.CharField(max_length = 1, default = 'O', choices = STATUS_CHOICE)
+
+
+
 
 
